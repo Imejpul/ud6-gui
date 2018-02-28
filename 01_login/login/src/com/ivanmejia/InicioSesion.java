15190b53e1a9b1cc61d3586c5e3edd2910da9c68
Unknown <ivanmejiapulido@gmail.com>
2018-02-28 19:33:51 +0100

    Ejer 1 (100%_Testeado)

--- 01_login/login/src/com/ivanmejia/InicioSesion.java
+++ 01_login/login/src/com/ivanmejia/InicioSesion.java
@@ -3,6 +3,7 @@ package com.ivanmejia;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class InicioSesion {
     private JPanel inicioSesion;
@@ -13,14 +14,30 @@ public class InicioSesion {
     private JLabel pass;
 
     //variables información usuario
-    private final String user = "Ivan Mejia";
+    private final String user = "ivan";
     private final String contrasenya = "12345";
 
     public InicioSesion() {
         entrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //JOptionPane.showMessageDialog();
+
+                char[] pwd = new char[5];
+
+                //convertimos la contraseña por defecto a Array de caracteres
+                for (int i = 0; i < 5; i++) {
+                    pwd[i] = contrasenya.charAt(i);
+                }
+
+                if (usuarioCampo.getText().equalsIgnoreCase(user) && Arrays.equals(passCampo.getPassword(), pwd)) {
+
+                    JOptionPane.showMessageDialog(null, "OK", "Resultado", JOptionPane.INFORMATION_MESSAGE);
+
+                } else {
+
+                    JOptionPane.showMessageDialog(null, "ERROR", "Resultado", JOptionPane.ERROR_MESSAGE);
+                }
+
             }
         });
     }
